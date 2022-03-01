489e845e6624ce65df30ceca92fa6944d5ff6d7a
BT_1E10_12 <sample@bths.edu>
2022-03-01 12:33:06 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] seats = {{"Moiz", "Angie", "Taran", "Kelvin", "Kyler", "David", "WenHao Huang", "Nicole", "Jennifer", "Michelle", null, null},
+                {"Beckett", "Raymond", "Lucy", "Apramjot", "Justin Lema", "Sam", "Tristan", "Pradeep", "Mohammad", "Haley", null, "Rely"},
+                {"Cheng Han", "Qihan", "Kevin", "Ryan", "Justin Liu", "Jeffrey", "Danny", "Elliot", "Benson", "Fiona", "Neil", "Kaitlyn"}};
+
+        int[][] seatNum = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                           {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22},
+                           {23, 24, 25, 26, 27, 28, 29 ,30, 31, 32, 33, 34}};
+
+        
+    }
+}
